ec85b2bcef33a9eeda8555b2e704a9d31acc2edb
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-06-02 11:17:18 +0500

    Added Hero Section Responsive

--- app/Hero/Page.jsx
+++ app/Hero/Page.jsx
@@ -6,7 +6,7 @@ import Topbar from "@/components/topbar";
 const HeroPage = () => {
   return (
     <div
-      className="w-full h-screen  bg-cover max-md:bg-center  bg-top-right bg-no-repeat"
+      className="w-full md:h-screen h-[600px]  bg-cover max-sm:bg-bottom-right max-sm:h-[500px]   bg-top-right bg-no-repeat"
       style={{ backgroundImage: `url(${HeroImage.src})` }}
     >
       <div className="w-full h-[40px] sm:block hidden fixed top-0 left-0 shadow-md z-50">
@@ -17,16 +17,16 @@ const HeroPage = () => {
       </div>
       <div className="relative w-full md:h-screen h-auto flex justify-center items-center">
         <div className="overlay bg-black/15 absolute top-0 left-0 h-screen w-full"></div>
-        <div className="content w-full h-screen absolute top-0 left-0 z-20 flex justify-center items-center">
+        <div className="content w-full md:h-screen h-[700px] max-sm:h-[500px] absolute top-0 left-0 z-20 flex justify-center items-center">
           <div className="md:w-[85%] w-full md:px-0 px-3 h-auto flex items-center">
-            <div className="lg:w-[50%] md:w-[70%] sm:w-full h-fit flex-col flex gap-4  text-white">
-              <h3 className="lg:text-xl -mb-7 md:text-2xl sm:text-xl xs:text-lg text-base font-normal urbanist text-[#DB3E22]">
+            <div className="lg:w-[50%] max-w-xl md:w-[70%] sm:w-full h-fit flex-col flex gap-4  text-white">
+              <h3 className="lg:text-xl  md:-mb-7 md:text-2xl sm:text-xl xs:text-lg text-base font-normal urbanist text-[#DB3E22]">
                 TASTE IN EVERY BITE
               </h3>
-              <h1 className="2xl:text-8xl b xl:text-7xl sm:text-5xl text-4xl md:leading-18 sm:leading-14 staatliches  leading-12 font-bold xl:leading-30">
+              <h1 className="2xl:text-8xl md:text-7xl text-6xl md:leading-18 sm:leading-14 staatliches  leading-12 font-bold xl:leading-30">
                 HOT & SPICY CHICKEN BURGER
               </h1>
-            <div className="button">
+            <div className="button my-5">
                 <Button className="px-9">Veiw Menu</Button>
             </div>
             </div>
