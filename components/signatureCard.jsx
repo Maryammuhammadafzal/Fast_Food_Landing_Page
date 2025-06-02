7a7d775e63eb0564ba937a49ebb98314c89d288b
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-06-02 15:36:09 +0500

    Created Signature Section

--- components/signatureCard.jsx
+++ components/signatureCard.jsx
@@ -1,9 +1,6 @@
 "use client";
-import PopularImage1 from "../public/signature-image1.png";
 import SignatureImage1 from "../public/signature-image1.png";
-import PopularImage2 from "../public/signature-image2.png";
 import SignatureImage2 from "../public/signature-image2.png";
-import PopularImage3 from "../public/signature-image3.png";
 import SignatureImage3 from "../public/signature-image3.png";
 import PopularBgImage from "../public/popular-bg.png";
 import { IoMdHeart } from "react-icons/io";
@@ -14,7 +11,7 @@ import { Button } from "./ui/button";
 import { useState } from "react";
 
 const SignatureCard = () => {
- const features_data = [
+  const features_data = [
     { id: 1, name: "Card 1" },
     { id: 2, name: "Card 2" },
     { id: 3, name: "Card 3" },
@@ -28,7 +25,6 @@ const SignatureCard = () => {
       [id]: !prev[id],
     }));
   };
-        
 
   let signature_Card_data = [
     {
@@ -62,77 +58,120 @@ const SignatureCard = () => {
             showStatus={false}
             showIndicators={true}
             infiniteLoop={true}
-            className="w-full  h-auto"
+            className="w-auto  h-auto"
           >
             <div
-              className="popular-card mx-auto w-[300px] h-[400px] rounded-lg relative font-['Jost'] bg-cover bg-center bg-[#FFB300]"
+              className="popular-card w-[360px] h-[470px] rounded-lg mx-auto relative font-['Jost'] bg-cover bg-center bg-[#008141]"
               style={{ backgroundImage: `url(${PopularBgImage.src})` }}
             >
               {/* Image Container */}
-              <div className="w-full h-full p-3 flex flex-col gap-5 items-center">
+              <div className="w-full h-full p-3 flex flex-col gap-5 text-white items-center ">
+                <div
+                  className="w-full h-auto flex"
+                  id="heart"
+                  onClick={() => toggleLike(0)}
+                >
+                  {likedCards[0] ? (
+                    <IoMdHeart size={35} />
+                  ) : (
+                    <IoIosHeartEmpty size={35} />
+                  )}
+                </div>
                 <img
-                  src={PopularImage1.src}
+                  src={SignatureImage1.src}
                   alt="card"
-                  className="w-[270px] h-[230px]"
+                  className="w-[200px] h-[180px]"
                 />
 
                 {/* Content */}
                 <div className="flex flex-col justify-center gap-2 items-center w-full h-auto">
-                  <h3 className="text-xl font-bold bg-white zilla uppercase text-[#FFB300]">
-                    Best Beef Burger
+                  <h3 className="text-xl font-bold zilla-slab-highlight uppercase text-white bg-[#008141]">
+                    BEST BEEF BURGER
                   </h3>
                   <p className="text-sm text-center text-white urbanist">
                     A team of dreamers and doers building unique interactive
                     music and art.
                   </p>
+                  <p className="text-lg text-white urbanist">Price: $9.99</p>
+                  <div className="button my-5">
+                    <Button className="px-16 h-8 urbanist text-sm rounded-none bg-[#FFB300]">
+                      Add To Cart
+                    </Button>
+                  </div>
                 </div>
               </div>
             </div>
             <div
-              className="popular-card mx-auto w-[300px] h-[400px] rounded-lg relative font-['Jost'] bg-cover bg-center bg-[#FFB300]"
+              className="popular-card w-[360px] h-[470px] rounded-lg mx-auto relative font-['Jost'] bg-cover bg-center bg-[#008141]"
               style={{ backgroundImage: `url(${PopularBgImage.src})` }}
             >
               {/* Image Container */}
-              <div className="w-full h-full p-3 flex flex-col gap-5 items-center">
-                <img
-                  src={PopularImage2.src}
-                  alt="card"
-                  className="w-[270px] h-[230px]"
-                />
+              <div className="w-full h-full p-3 flex flex-col gap-5 text-white items-center ">
+                <div
+                  className="w-full h-auto flex"
+                  id="heart"
+                  onClick={() => toggleLike(1)}
+                >
+                  {likedCards[1] ? (
+                    <IoMdHeart size={35} />
+                  ) : (
+                    <IoIosHeartEmpty size={35} />
+                  )}
+                </div>
+                <img src={SignatureImage2.src} alt="card" className="w-[200px] h-[180px]" />
 
                 {/* Content */}
                 <div className="flex flex-col justify-center gap-2 items-center w-full h-auto">
-                  <h3 className="text-xl font-bold bg-white zilla uppercase text-[#FFB300]">
+                  <h3 className="text-xl font-bold zilla-slab-highlight uppercase text-white bg-[#008141]">
                     ALFAREDO PASTA
                   </h3>
                   <p className="text-sm text-center text-white urbanist">
                     A team of dreamers and doers building unique interactive
                     music and art.
                   </p>
+                  <p className="text-lg text-white urbanist">Price: $9.99</p>
+                  <div className="button my-5">
+                    <Button className="px-16 h-8 urbanist text-sm rounded-none bg-[#FFB300]">
+                      Add To Cart
+                    </Button>
+                  </div>
                 </div>
               </div>
             </div>
             <div
-              className="popular-card mx-auto w-[300px] h-[400px] rounded-lg relative font-['Jost'] bg-cover bg-center bg-[#FFB300]"
+              className="popular-card w-[360px] h-[470px] rounded-lg relative mx-auto font-['Jost'] bg-cover bg-center bg-[#008141]"
               style={{ backgroundImage: `url(${PopularBgImage.src})` }}
             >
               {/* Image Container */}
-              <div className="w-full h-full p-3 flex flex-col gap-5 items-center">
-                <img
-                  src={PopularImage3.src}
-                  alt="card"
-                  className="w-[270px] h-[230px]"
-                />
+              <div className="w-full h-full p-3 flex flex-col gap-5 text-white items-center ">
+                <div
+                  className="w-full h-auto flex"
+                  id="heart"
+                  onClick={() => toggleLike(2)}
+                >
+                  {likedCards[2] ? (
+                    <IoMdHeart size={35} />
+                  ) : (
+                    <IoIosHeartEmpty size={35} />
+                  )}
+                </div>
+                <img src={SignatureImage3.src} alt="card" className="w-[200px] h-[180px]" />
 
                 {/* Content */}
                 <div className="flex flex-col justify-center gap-2 items-center w-full h-auto">
-                  <h3 className="text-xl font-bold bg-white zilla uppercase text-[#FFB300]">
+                  <h3 className="text-xl font-bold zilla-slab-highlight uppercase text-white bg-[#008141]">
                     DELICIOUS PIZZA
                   </h3>
                   <p className="text-sm text-center text-white urbanist">
                     A team of dreamers and doers building unique interactive
                     music and art.
                   </p>
+                  <p className="text-lg text-white urbanist">Price: $9.99</p>
+                  <div className="button my-5">
+                    <Button className="px-16 h-8 urbanist text-sm rounded-none bg-[#FFB300]">
+                      Add To Cart
+                    </Button>
+                  </div>
                 </div>
               </div>
             </div>
@@ -149,8 +188,16 @@ const SignatureCard = () => {
               >
                 {/* Image Container */}
                 <div className="w-full h-full p-3 flex flex-col gap-5 text-white items-center ">
-                  <div className="w-full h-auto flex" id="heart" onClick={() => toggleLike(index)}>
-                   {likedCards[index] ? <IoMdHeart size={35} /> : <IoIosHeartEmpty size={35} />}
+                  <div
+                    className="w-full h-auto flex"
+                    id="heart"
+                    onClick={() => toggleLike(index)}
+                  >
+                    {likedCards[index] ? (
+                      <IoMdHeart size={35} />
+                    ) : (
+                      <IoIosHeartEmpty size={35} />
+                    )}
                   </div>
                   <img src={image} alt="card" className="w-[200px] h-[180px]" />
 
