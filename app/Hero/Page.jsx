17d627a063d3e52e7292057bbb92c3fc757dbf21
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-06-02 10:32:32 +0500

    Added

--- app/Hero/Page.jsx
+++ app/Hero/Page.jsx
@@ -2,18 +2,19 @@ import Header from "@/components/header";
 import React from "react";
 import HeroImage from "../../public/hero-image.png"
 import { Button } from "@/components/ui/button";
+import Topbar from "@/components/topbar";
 const HeroPage = () => {
   return (
     <div
       className="w-full h-screen  bg-cover max-md:bg-center  bg-top-right bg-no-repeat"
       style={{ backgroundImage: `url(${HeroImage.src})` }}
     >
-      <div className="w-full md:h-[90px] h-auto md:py-0 py-5 bg-[#333333] fixed top-0 left-0 shadow-md z-50">
+      <div className="w-full md:h-[40px] h-auto md:py-0 py-5 fixed top-0 left-0 shadow-md z-50">
         <Topbar />
       </div>
-      <div className="w-full md:h-[90px] h-auto md:py-0 py-5 bg-[#333333] absolute top-0 left-0 shadow-md z-50">
+      {/* <div className="w-full md:h-[90px] h-auto md:py-0 py-5 bg-[#333333] absolute top-0 left-0 shadow-md z-50">
         <Header />
-      </div>
+      </div> */}
       <div className="relative w-full md:h-screen h-auto flex justify-center items-center">
         <div className="overlay bg-black/15 absolute top-0 left-0 h-screen w-full"></div>
         <div className="content w-full h-screen absolute top-0 left-0 z-20 flex justify-center items-center">
--- components/topbar.jsx
+++ components/topbar.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import { TbTruckDelivery } from "react-icons/tb";
+ import { FaFacebook } from "react-icons/fa"; 
+ import { TbBrandWhatsappFilled } from "react-icons/tb";
+ import { FaSquareInstagram } from "react-icons/fa6";
+ import { FaXTwitter } from "react-icons/fa6";
+ import { ImLinkedin } from "react-icons/im";
 const Topbar = () => {
   return (
-    <div>
-      
+    <div className='w-full h-[40px] bg-green-700 flex justify-center items-center'>
+      <div className="w-[85%] h-auto flex justify-between items-center">
+        <div className="left w-auto flex gap-5 text-sm text-white">
+                <p>100% Secure delivery without contacting the courier</p>
+                <p className='flex gap-5'><TbTruckDelivery size={20} /> Track Your Order</p>
+        </div>
+        <div className="left w-auto flex gap-5 text-white">
+                <FaFacebook size={15} />
+                <FaXTwitter size={15} />
+                <ImLinkedin size={15} />
+                <FaSquareInstagram size={15} />
+                <TbBrandWhatsappFilled size={15} />
+        </div>
+      </div>
     </div>
   )
 }
