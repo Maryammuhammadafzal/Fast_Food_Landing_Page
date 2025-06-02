0df486b0308f76708a68840c17784865867575b9
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-06-02 11:57:58 +0500

    Added Features Section

--- app/Features/Page.jsx
+++ app/Features/Page.jsx
@@ -1,15 +1,65 @@
-import React from 'react'
-import FeaturesBgImage from '../../public/features-bg.png'
+import React from "react";
+import FeaturesBgImage from "../../public/features-bg.png";
+import { TbTruckDelivery } from "react-icons/tb";
+import { MdFastfood } from "react-icons/md";
+import { PiNotebookBold } from "react-icons/pi";
+import { GiChickenOven } from "react-icons/gi";
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
+
 const FeaturesPage = () => {
+  let features_data = [
+    {
+      icon: MdFastfood,
+      title: "super quality food",
+      description:
+        "A team of dreamers and doers building unique interactive music and art.",
+    },
+    {
+      icon: MdFastfood,
+      title: "original recipes",
+      description:
+        "A team of dreamers and doers building unique interactive music and art.",
+    },
+    {
+      icon: MdFastfood,
+      title: "quick fast delivery",
+      description:
+        "A team of dreamers and doers building unique interactive music and art.",
+    },
+    {
+      icon: MdFastfood,
+      title: "100% fresh foods",
+      description:
+        "A team of dreamers and doers building unique interactive music and art.",
+    },
+  ];
   return (
-    <div className='w-full flex justify-center items-center my-12 h-auto'>
-      <div className="w-[85%] flex justify-center items-center rounded-2xl bg-cover bg-center bg-[#242424] h-[250px]" 
-      style={{backgroundImage : `url(${FeaturesBgImage.src})`}}>
-
+    <div className="w-full flex justify-center items-center my-12 h-auto">
+      <div
+        className=" max-w-7xl w-[85%] px-5 flex justify-center items-center  rounded-2xl bg-cover bg-center bg-[#242424] h-auto lg:h-[250px]"
+        style={{ backgroundImage: `url(${FeaturesBgImage.src})` }}
+      >
+        <div className="cards w-full max-lg:h-auto max-lg:flex-wrap flex gap-4 justify-center sm:justify-between items-center">
+          {features_data.map(({ title, description }, index) => (
+            <Card key={index} className="lg:w-[250px] sm:w-[45%] h-auto flex flex-col gap-3 bg-transparent border-none">
+              <CardContent className="text-white flex flex-col gap-3 px-1">
+                {index === 0 && <MdFastfood size={40} className="text-red-700" /> || index === 1 && <PiNotebookBold size={40} className="text-red-700" /> || index === 2 && <TbTruckDelivery size={40} className="text-red-700" /> || index === 3 && <GiChickenOven size={40} className="text-red-700" />}
+                
+                <div>
+                  <h3 className="staatliches font-normal text-xl capatalize">
+                    {title}
+                  </h3>
+                  <p className="urbanist font-normal text-base">
+                   {description}
+                  </p>
+                </div>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default FeaturesPage
+export default FeaturesPage;
--- app/Hero/Page.jsx
+++ app/Hero/Page.jsx
@@ -16,7 +16,7 @@ const HeroPage = () => {
         <Header />
       </div>
       <div className="relative w-full md:h-screen h-auto flex justify-center items-center">
-        <div className="overlay bg-black/15 absolute top-0 left-0 h-screen w-full"></div>
+        <div className="overlay bg-black/15 absolute top-0 left-0 md:h-screen max-md:h-[600px] max-sm:h-[500px] w-full"></div>
         <div className="content w-full  h-screen max-md:h-[700px] max-sm:h-[500px] absolute top-0 left-0 z-20 flex justify-center items-center">
           <div className="md:w-[85%] w-full md:px-0 px-3 h-auto md:pt-30 flex items-center">
             <div className="lg:w-[50%] max-w-xl md:w-[70%] sm:w-full h-fit flex-col flex gap-4  text-white">
