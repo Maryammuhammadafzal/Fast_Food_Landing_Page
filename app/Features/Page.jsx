97d865addce9742be03346930e7276ed91ea3dc4
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-06-02 12:06:47 +0500

    Adding Popular Section

--- app/Features/Page.jsx
+++ app/Features/Page.jsx
@@ -36,21 +36,36 @@ const FeaturesPage = () => {
   return (
     <div className="w-full flex justify-center items-center my-12 h-auto">
       <div
-        className=" max-w-7xl w-[85%] px-5 flex justify-center items-center  rounded-2xl bg-cover bg-center bg-[#242424] h-auto lg:h-[250px]"
+        className=" max-w-7xl w-[85%] px-5 flex justify-center relative items-center  rounded-2xl -z-20 bg-cover bg-center bg-[#242424] h-auto lg:h-[250px]"
         style={{ backgroundImage: `url(${FeaturesBgImage.src})` }}
       >
+        <div className="absolute top-0 left-0 bg-black/30 h-full w-full -z-10"></div>
         <div className="cards w-full max-lg:h-auto max-lg:flex-wrap flex gap-4 justify-center sm:justify-between items-center">
           {features_data.map(({ title, description }, index) => (
-            <Card key={index} className="lg:w-[250px] sm:w-[45%] h-auto flex flex-col gap-3 bg-transparent border-none">
+            <Card
+              key={index}
+              className="lg:w-[250px] sm:w-[45%] h-auto flex flex-col gap-3 bg-transparent border-none"
+            >
               <CardContent className="text-white flex flex-col gap-3 px-1">
-                {index === 0 && <MdFastfood size={40} className="text-red-700" /> || index === 1 && <PiNotebookBold size={40} className="text-red-700" /> || index === 2 && <TbTruckDelivery size={40} className="text-red-700" /> || index === 3 && <GiChickenOven size={40} className="text-red-700" />}
-                
+                {(index === 0 && (
+                  <MdFastfood size={50} className="text-red-700" />
+                )) ||
+                  (index === 1 && (
+                    <PiNotebookBold size={50} className="text-red-700" />
+                  )) ||
+                  (index === 2 && (
+                    <TbTruckDelivery size={50} className="text-red-700" />
+                  )) ||
+                  (index === 3 && (
+                    <GiChickenOven size={50} className="text-red-700" />
+                  ))}
+
                 <div>
                   <h3 className="staatliches font-normal text-xl capatalize">
                     {title}
                   </h3>
                   <p className="urbanist font-normal text-base">
-                   {description}
+                    {description}
                   </p>
                 </div>
               </CardContent>
---
+++ components/poularCard.jsx
@@ -0,0 +1,140 @@
+import React from 'react'
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+const poularCard = () => {
+  return (
+     <>
+      {window.innerWidth <= 768 ? (
+        <>
+          <Carousel
+            showArrows={true}
+            showStatus={false}
+            showIndicators={true}
+            infiniteLoop={true}
+            className="w-full  h-auto"
+          >
+            <div className="popular-card group w-[270px] h-[320px] mb-4 mx-auto rounded-lg overflow-hidden relative font-['Jost']">
+              {/* Image Container */}
+              <div className="relative w-[250px] h-[300px]">
+                <img
+                  src={CardImage1}
+                  alt="card"
+                  className="w-[270px] h-[320px] object-cover"
+                />
+
+                {/* Overlay (appears on hover) */}
+                {/* <div className="absolute inset-0 bg-black opacity-15 top-4 z-10"></div>
+                <div className="absolute inset-0 bg-red-700 h-0 group-hover:h-[150px] transition-h duration-300 top-40  z-10"></div> */}
+              </div>
+
+              {/* Content */}
+              <div className="absolute inset-0 z-20 flex flex-col justify-end pb-10 pl-3 pr-6 text-white">
+                <h3 className="text-lg font-bold">Truffle Mushroom Risotto"</h3>
+                <p className="text-sm">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                </p>
+              </div>
+            </div>
+            <div className="popular-card group w-[270px] h-[320px] mx-auto rounded-lg overflow-hidden relative font-['Jost']">
+              {/* Image Container */}
+              <div className="relative w-[250px] h-[300px]">
+                <img
+                  src={CardImage1}
+                  alt="card"
+                  className="w-[270px] h-[320px] object-cover"
+                />
+
+                {/* Overlay (appears on hover) */}
+                {/* <div className="absolute inset-0 bg-black opacity-15 top-4 z-10"></div>
+                <div className="absolute inset-0 bg-red-700 h-0 group-hover:h-[150px] transition-h duration-300 top-40  z-10"></div> */}
+              </div>
+
+              {/* Content */}
+              <div className="absolute inset-0 z-20 flex flex-col justify-end pb-10 pl-3 pr-6 text-white">
+                <h3 className="text-lg font-bold">Truffle Mushroom Risotto"</h3>
+                <p className="text-sm">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                </p>
+              </div>
+            </div>
+            <div className="popular-card group w-[270px] h-[320px] mx-auto rounded-lg overflow-hidden relative font-['Jost']">
+              {/* Image Container */}
+              <div className="relative w-[250px] h-[300px]">
+                <img
+                  src={CardImage1}
+                  alt="card"
+                  className="w-[270px] h-[320px] object-cover"
+                />
+
+                {/* Overlay (appears on hover) */}
+                {/* <div className="absolute inset-0 bg-black opacity-15 top-4 z-10"></div>
+                <div className="absolute inset-0 bg-red-700 h-0 group-hover:h-[150px] transition-h duration-300 top-40  z-10"></div> */}
+              </div>
+
+              {/* Content */}
+              <div className="absolute inset-0 z-20 flex flex-col justify-end pb-10 pl-3 pr-6 text-white">
+                <h3 className="text-lg font-bold">Truffle Mushroom Risotto"</h3>
+                <p className="text-sm">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                </p>
+              </div>
+            </div>
+            <div className="popular-card group w-[270px] h-[320px] mx-auto rounded-lg overflow-hidden relative font-['Jost']">
+              {/* Image Container */}
+              <div className="relative w-[250px] h-[300px]">
+                <img
+                  src={CardImage1}
+                  alt="card"
+                  className="w-[270px] h-[320px] object-cover"
+                />
+
+                {/* Overlay (appears on hover) */}
+                {/* <div className="absolute inset-0 bg-black opacity-15 top-4 z-10"></div>
+                <div className="absolute inset-0 bg-red-700 h-0 group-hover:h-[150px] transition-h duration-300 top-40  z-10"></div> */}
+              </div>
+
+              {/* Content */}
+              <div className="absolute inset-0 z-20 flex flex-col justify-end pb-10 pl-3 pr-6 text-white">
+                <h3 className="text-lg font-bold">Truffle Mushroom Risotto"</h3>
+                <p className="text-sm">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                </p>
+              </div>
+            </div>
+          </Carousel>
+        </>
+      ) : (
+        <>
+          {popular_card_data.map(
+            ({ card_image, card_title, card_description }) => (
+              <div key={card_title} className="popular-card w-[300px] h-[320px] rounded-lg overflow-hidden relative font-['Jost']">
+                {/* Image Container */}
+
+                <img
+                  src={card_image}
+                  alt="card"
+                  className="w-[300px] h-[380px] object-cover"
+                />
+
+                {/* Content */}
+                <div className="absolute bottom-0 z-20 flex flex-col justify-end hover:animate-fade-in-background-color w-full h-[120px] text-white">
+                  <div className="relative w-full h-full p-6">
+                    {/* Overlay (appears on hover) */}
+                    {/* <div className="absolute bg-black bottom-0 left-0 z-10"></div>
+                    <div className="absolute bg-red-700 h-0 hover:h-[150px] transition-h duration-300 bottom-0 left-0 z-10"></div> */}
+
+                    <h3 className="text-lg font-bold">{card_title}</h3>
+                    <p className="text-sm">{card_description}</p>
+                  </div>
+                </div>
+              </div>
+            )
+          )}
+        </>
+      )}
+    </>
+  )
+}
+
+export default poularCard
