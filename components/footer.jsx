4efad14a5e5752907b7f4719fac198a15cb4af54
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-06-03 14:14:12 +0500

    done

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { FaArrowRightLong } from 'react-icons/fa6';
+import React from "react";
+import { FaArrowRightLong } from "react-icons/fa6";
 import FeaturesBgImage from "../public/features-bg.png";
- import { FaFacebook } from "react-icons/fa"; 
- import { TbBrandWhatsappFilled } from "react-icons/tb";
- import { FaSquareInstagram } from "react-icons/fa6";
- import { FaXTwitter } from "react-icons/fa6";
- import { ImLinkedin } from "react-icons/im";
+import { FaFacebook } from "react-icons/fa";
+import { TbBrandWhatsappFilled } from "react-icons/tb";
+import { FaSquareInstagram } from "react-icons/fa6";
+import { FaXTwitter } from "react-icons/fa6";
+import { ImLinkedin } from "react-icons/im";
 const Footer = () => {
-   let contact_data = [
+  let contact_data = [
     {
       name: "Address:",
       link: "3891 Ranchview Dr. Richardson, California 62639",
@@ -23,13 +23,14 @@ const Footer = () => {
   ];
   return (
     <div className="w-full text-white h-auto gap-5 flex justify-center items-center p-3">
-      <div className="academySection  w-[95%] max-md:w-full bg-[#008141] rounded-2xl relative -z-20 h-auto flex flex-col gap-4 justify-center items-center ">
-        <div className="w-full h-full  absolute top-0 left-0  bg-cover bg-center opacity-30 -z-10"
-         style={{backgroundImage: `url(${FeaturesBgImage.src})`}}>
-          {/* <div className="overlay w-full h-full absolute top-0 left-0 bg-black"></div> */}
-         </div>
-        <div className="footerContent max-sm:justify-center w-[90%] py-10 h-auto flex flex-wrap justify-between mb-3 ">
-          <div className="left w-[35%] max-sm:w-[100%] max-[900px]:w-[45%] max-sm:flex max-sm:flex-col max-sm:justify-center max-sm:items-center p-3 h-auto">
+      <div className=" w-[95%] max-md:w-full bg-[#008141] rounded-2xl relative -z-20 h-auto flex flex-col gap-4 justify-center items-center ">
+        <div
+          className="w-full h-full  absolute top-0 left-0  bg-cover bg-center opacity-30 -z-10"
+          style={{ backgroundImage: `url(${FeaturesBgImage.src})` }}
+        >
+        </div>
+        <div className="footerContent max-sm:justify-center xl:w-[85%] py-10 h-auto flex flex-wrap justify-between mb-3 ">
+          <div className="left w-[35%] max-sm:w-[100%] max-[900px]:w-[45%] max-sm:flex max-sm:flex-col max-sm:justify-center justify-start max-sm:items-center p-3 h-auto">
             {/* Logo */}
             <div className="logo ">
               <h3 className="font-extrabold text-4xl text-yellow-400 staatliches tracking-wide uppercase">
@@ -56,7 +57,7 @@ const Footer = () => {
               </li>
               <li className="text-lg font-[Nunito Sans]  font-[400]">
                 <a href="#" className="text-lg">
-                 Menu
+                  Menu
                 </a>
               </li>
               <li className="text-lg font-[Nunito Sans]  font-[400]">
@@ -76,11 +77,11 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-          <div className="middle max-sm:w-auto max-sm:mx-auto w-[20%] p-3 h-auto">
+          <div className="middle max-sm:w-auto max-sm:mx-auto w-[25%] p-3 h-auto">
             <h4 className=" text-yellow-400 staatliches text-2xl font-bold capitalize max-lg:text-xl max-md:text-lg">
               Get In Touch
             </h4>
-            
+
             <ul className="list-none py-2 mt-5 gap-2 flex flex-col ">
               <li className="text-lg font-[Nunito Sans]  font-[400] ">
                 <a href="#" className="text-lg">
@@ -94,27 +95,27 @@ const Footer = () => {
               </li>
               <li className="text-lg font-[Nunito Sans]  font-[400]">
                 <a href="#" className="text-lg">
-                (308) 555-0121
+                  (308) 555-0121
                 </a>
               </li>
             </ul>
           </div>
-          <div className="left max-sm:w-full max-sm:px-3  w-[25%] p-3  h-auto max-[900px]:w-full">
+          <div className="left max-sm:w-full max-sm:px-3   w-[20%] p-3  h-auto max-[900px]:w-full">
             <h4 className=" text-yellow-400 staatliches text-2xl font-bold capitalize max-lg:text-xl  max-md:text-lg">
               Contact
             </h4>
-            <div className="left w-auto flex flex-wrap gap-5 text-white">
-                            <FaFacebook size={15} />
-                            <FaXTwitter size={15} />
-                            <ImLinkedin size={15} />
-                            <FaSquareInstagram size={15} />
-                            <TbBrandWhatsappFilled size={15} />
-                    </div>
+            <div className="left w-auto mt-10 flex flex-wrap gap-6 xl:px-6  text-white">
+              <FaFacebook size={30} />
+              <FaXTwitter size={30} />
+              <ImLinkedin size={30} />
+              <FaSquareInstagram size={30} />
+              <TbBrandWhatsappFilled size={30} />
+            </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
